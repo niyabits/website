3b282d0e18b9b4b17ccb823854b3858886db49b8
Yash Gupta <yashguptaofficial@gmail.com>
2019-01-30 19:17:14 +0530

    new

--- smooth.js
+++ smooth.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
+  $('a').on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== '') {
       // Prevent default anchor click behavior
       event.preventDefault();
 
@@ -12,13 +11,16 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 500, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
     } // End if
   });
 });
